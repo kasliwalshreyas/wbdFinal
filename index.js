5a67be09497319250dcdf5c028297fee7356dc02
naman42624 <75940862+naman42624@users.noreply.github.com>
2023-04-20 02:17:47 +0530

    bugs

--- index.js
+++ index.js
@@ -21,13 +21,22 @@ io.on("connection", (socket)=>{
     })
     socket.on("send-message", (data) => {
         const { receiverId } = data;
+        const { senderId } = data;
         const user = activeUSers.find((user) => user.userId === receiverId);
+        const cur_user = activeUSers.find((user) => user.userId === senderId);
         console.log("Sending from socket to :", receiverId)
         console.log("Data: ", data)
         if (user) {
-          io.to(user.socketId).emit('recieve-message', data);
-        // io.emit('recieve-message', data);
+            // user.receiverId.join("rec")
+            // socket.join(user.receiverId)
+
+        //   io.to(user.socketId).emit('recieve-message', data);
+        //   io.to(user.socketId).emit('recieve-message', data);
+        //   io.to(user.socketId).emit('recieve-message', data)
+            // io.to(user.receiverId).emit('recieve-message', data)
+        io.emit('recieve-message', data);
           console.log("sent to ",user.socketId)
+          console.log("from ", cur_user.socketId)
         }
       });
     socket.on("disconnect",()=>{
